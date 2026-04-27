74b57da43ccea7043557d2bcd4d184fbe844c0c2
Hanaka15 <hanakakiller@gmail.com>
2026-04-27 18:13:49 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: cloudflare()
 });
