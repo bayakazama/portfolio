b0b1b22cb1f224415b80aa8788f7335504895fc6
Hanaka15 <hanakakiller@gmail.com>
2026-04-27 17:41:41 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare"
 import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
